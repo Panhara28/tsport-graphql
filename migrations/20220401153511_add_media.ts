8b52aac692a2e631f69406dccbdd0152f3f8d50d
aipanhara <titpanhara@gmail.com>
2022-04-01 16:12:05 +0700

    fix

---
+++ migrations/20220401153511_add_media.ts
@@ -0,0 +1,16 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  if (!(await knex.schema.hasTable('media'))) {
+    return await knex.schema.createTable('media', function(table) {
+      table.increments();
+      table.string('image_url');
+      table.integer('website_id');
+      table.integer('created_by');
+      table.integer('updated_by');
+      table.timestamps(true, true);
+    });
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {}
