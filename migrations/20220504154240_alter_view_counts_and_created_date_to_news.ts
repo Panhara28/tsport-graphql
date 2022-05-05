abb3c029caccdf3699d8fab4243868c25b16bf52
Reachea <reacheasambath@gmail.com>
2022-05-05 15:02:59 +0700

    remove count

--- migrations/20220504154240_alter_view_counts_and_created_date_to_news.ts
+++ migrations/20220504154240_alter_view_counts_and_created_date_to_news.ts
@@ -3,7 +3,6 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return await knex.schema.table('news', field => {
     field.dateTime('created_date');
-    field.integer('view_counts').defaultTo(0);
   });
 }
 
