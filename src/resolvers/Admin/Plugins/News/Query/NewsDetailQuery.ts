904c394ad8745399631bfcd6d8b8e692e59bfe3a
aipanhara <titpanhara@gmail.com>
2022-05-13 14:53:07 +0700

    fix

--- src/resolvers/Admin/Plugins/News/Query/NewsDetailQuery.ts
+++ src/resolvers/Admin/Plugins/News/Query/NewsDetailQuery.ts
@@ -5,21 +5,33 @@ import ContextType from 'src/graphql/ContextType';
 export const NewsDetailQuery = async (_, { id, websiteId }: { id: number; websiteId: number }, ctx: ContextType) => {
   const knex = ctx.knex.default;
   await ctx.authUser.requireLogin('USER');
-  const isRead = await ctx.authUser.user.read;
-  if (isRead) {
-    const newsDetail = await knex
-      .table('news')
-      .where({ id })
-      .andWhere('website_id', '=', websiteId)
-      .first();
+  const newsDetail = await knex
+    .table('news')
+    .where({ id })
+    .andWhere('website_id', '=', websiteId)
+    .first();
 
-    return {
-      ...newsDetail,
-      // created_date: toKhmerFormat(newsDetail?.created_date),
-      published_date: newsDetail?.published_date ? toKhmerFormat(newsDetail?.published_date) : undefined,
-      description: newsDetail.description ? newsDetail.description : undefined,
-    };
-  } else {
-    throw new AuthenticationError(`You don't have permission`);
-  }
+  return {
+    ...newsDetail,
+    // created_date: toKhmerFormat(newsDetail?.created_date),
+    published_date: newsDetail?.published_date ? toKhmerFormat(newsDetail?.published_date) : undefined,
+    description: newsDetail.description ? newsDetail.description : undefined,
+  };
+  // const isRead = await ctx.authUser.user.read;
+  // if (isRead) {
+  //   const newsDetail = await knex
+  //     .table('news')
+  //     .where({ id })
+  //     .andWhere('website_id', '=', websiteId)
+  //     .first();
+
+  //   return {
+  //     ...newsDetail,
+  //     // created_date: toKhmerFormat(newsDetail?.created_date),
+  //     published_date: newsDetail?.published_date ? toKhmerFormat(newsDetail?.published_date) : undefined,
+  //     description: newsDetail.description ? newsDetail.description : undefined,
+  //   };
+  // } else {
+  //   throw new AuthenticationError(`You don't have permission`);
+  // }
 };
