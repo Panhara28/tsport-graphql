a7030e7500412d470a016a404cb52fd76a229cf2
mocdevs <it.support@moc.gov.kh>
2022-05-16 21:09:41 +0700

    fix apollo client

--- src/resolvers/Authentication/Query/AdminMeQuery.ts
+++ src/resolvers/Authentication/Query/AdminMeQuery.ts
@@ -1,11 +1,14 @@
 import { AuthenticationError } from 'apollo-server';
 import ContextType from '../../../graphql/ContextType';
 
-export const AdminMeQuery = async (_, { websiteId }: { websiteId: number }, ctx: ContextType) => {
+export const AdminMeQuery = async (
+  _,
+  { websiteId, clientToken }: { websiteId: number; clientToken: string },
+  ctx: ContextType,
+) => {
   const knex = ctx.knex.default;
-  const token = ctx.authUser.user.token;
 
-  if (token) {
+  if (clientToken) {
     const queryUser = knex
       .table('user_token')
       .innerJoin('users', 'users.id', 'user_token.user_id')
@@ -28,7 +31,7 @@ export const AdminMeQuery = async (_, { websiteId }: { websiteId: number }, ctx:
         'website_user_details.website_id as websiteId',
         'users.profile_picture',
       )
-      .where({ token })
+      .where({ token: clientToken })
       .first();
     // Query current user by websiteId
     if (websiteId) {
