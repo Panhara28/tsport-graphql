7eb2094f3cac403452c5e0ddb9c55edcf8697d36
mocdevs <it.support@moc.gov.kh>
2022-05-29 14:32:03 +0700

    fix

--- src/resolvers/Admin/Plugins/Media/Mutation/CreateMediaMutation.ts
+++ src/resolvers/Admin/Plugins/Media/Mutation/CreateMediaMutation.ts
@@ -8,24 +8,37 @@ export const CreateMediaMutation = async (
   ctx: ContextType,
 ) => {
   const knex = ctx.knex.default;
-  await ctx.authUser.requireLogin('USER');
-  const isWrite = await ctx.authUser.user.write;
-  const admin_id = await ctx.authUser.user.id;
+  // await ctx.authUser.requireLogin('USER');
+  // const isWrite = await ctx.authUser.user.write;
+  // const admin_id = await ctx.authUser.user.id;
 
-  if (isWrite) {
-    const [createMedia] = await knex.table('media').insert({
-      image_url: input.image_url,
-      upload_storage: input.upload_storage,
-      mimetype: input.mimetype,
-      width: input.width,
-      height: input.height,
-      website_id: websiteId,
-      created_by: admin_id,
-      updated_by: admin_id,
-    });
+  // if (isWrite) {
+  //   const [createMedia] = await knex.table('media').insert({
+  //     image_url: input.image_url,
+  //     upload_storage: input.upload_storage,
+  //     mimetype: input.mimetype,
+  //     width: input.width,
+  //     height: input.height,
+  //     website_id: websiteId,
+  //     created_by: admin_id,
+  //     updated_by: admin_id,
+  //   });
 
-    return createMedia;
-  } else {
-    throw new AuthenticationError(`You don't have permission`);
-  }
+  //   return createMedia;
+  // } else {
+  //   throw new AuthenticationError(`You don't have permission`);
+  // }
+
+  const [createMedia] = await knex.table('media').insert({
+    image_url: input.image_url,
+    upload_storage: input.upload_storage,
+    mimetype: input.mimetype,
+    width: input.width,
+    height: input.height,
+    website_id: websiteId,
+    created_by: 1,
+    updated_by: 1,
+  });
+
+  return createMedia;
 };
