39f6704a8b92c90e16bad6bafdccda68e07f6170
mocdevs <it.support@moc.gov.kh>
2022-05-29 14:25:17 +0700

    fix

--- src/resolvers/Upload/Mutation/UploadResolver.ts
+++ src/resolvers/Upload/Mutation/UploadResolver.ts
@@ -5,54 +5,96 @@ import { AuthenticationError } from 'apollo-server';
 import sizeOf from 'image-size';
 
 export const UploadResolver = async (_, { file }, ctx: ContextType) => {
-  await ctx.authUser.requireLogin('USER');
-  const isUserWrite = await ctx.authUser.user.write;
+  // await ctx.authUser.requireLogin('USER');
+  // const isUserWrite = await ctx.authUser.user.write;
+  const { createReadStream, filename, mimetype } = await file;
+  const stream = await createReadStream();
+  let fileSize = 0;
 
-  if (isUserWrite) {
-    const { createReadStream, filename, mimetype } = await file;
-    const stream = await createReadStream();
-    let fileSize = 0;
+  stream.on('end', function() {
+    fileSize = stream.bytesRead / (1024 * 1024); //Convert to MB
+    fileSize = Number(fileSize.toFixed(4));
 
-    stream.on('end', function() {
-      fileSize = stream.bytesRead / (1024 * 1024); //Convert to MB
-      fileSize = Number(fileSize.toFixed(4));
+    if (fileSize > 100) {
+      throw new AuthenticationError('You file is too large!');
+    }
+  });
 
-      if (fileSize > 100) {
-        throw new AuthenticationError('You file is too large!');
-      }
-    });
-
-    const json = await request.post({
-      url: process.env.S1 ? process.env.S1 + '/upload' : 'https://imagerium.blockerium.com' + '/upload',
-      formData: {
-        mocspace: {
-          value: await streamToBuffer(stream),
-          options: {
-            filename: filename,
-            contentType: mimetype,
-          },
+  const json = await request.post({
+    url: process.env.S1 ? process.env.S1 + '/upload' : 'https://imagerium.blockerium.com' + '/upload',
+    formData: {
+      mocspace: {
+        value: await streamToBuffer(stream),
+        options: {
+          filename: filename,
+          contentType: mimetype,
         },
       },
-      json: true,
-    });
-    let dimensions;
-    const extention = json.filename.split('/')[4].split('.')[1];
+    },
+    json: true,
+  });
+  let dimensions;
+  const extention = json.filename.split('/')[4].split('.')[1];
 
-    if (extention === 'pdf' || extention === 'docx' || extention === 'csv') {
-      // console.log('file');
-    } else {
-      dimensions = sizeOf(stream.path);
-    }
-
-    return {
-      filename,
-      url: json.filename,
-      fileSize,
-      mimetype,
-      width: dimensions?.width ? dimensions?.width : 0,
-      height: dimensions?.height ? dimensions?.height : 0,
-    };
+  if (extention === 'pdf' || extention === 'docx' || extention === 'csv') {
+    // console.log('file');
   } else {
-    throw new AuthenticationError(`You don't have permission!`);
+    dimensions = sizeOf(stream.path);
   }
+
+  return {
+    filename,
+    url: json.filename,
+    fileSize,
+    mimetype,
+    width: dimensions?.width ? dimensions?.width : 0,
+    height: dimensions?.height ? dimensions?.height : 0,
+  };
+  // if (isUserWrite) {
+  //   const { createReadStream, filename, mimetype } = await file;
+  //   const stream = await createReadStream();
+  //   let fileSize = 0;
+
+  //   stream.on('end', function() {
+  //     fileSize = stream.bytesRead / (1024 * 1024); //Convert to MB
+  //     fileSize = Number(fileSize.toFixed(4));
+
+  //     if (fileSize > 100) {
+  //       throw new AuthenticationError('You file is too large!');
+  //     }
+  //   });
+
+  //   const json = await request.post({
+  //     url: process.env.S1 ? process.env.S1 + '/upload' : 'https://imagerium.blockerium.com' + '/upload',
+  //     formData: {
+  //       mocspace: {
+  //         value: await streamToBuffer(stream),
+  //         options: {
+  //           filename: filename,
+  //           contentType: mimetype,
+  //         },
+  //       },
+  //     },
+  //     json: true,
+  //   });
+  //   let dimensions;
+  //   const extention = json.filename.split('/')[4].split('.')[1];
+
+  //   if (extention === 'pdf' || extention === 'docx' || extention === 'csv') {
+  //     // console.log('file');
+  //   } else {
+  //     dimensions = sizeOf(stream.path);
+  //   }
+
+  //   return {
+  //     filename,
+  //     url: json.filename,
+  //     fileSize,
+  //     mimetype,
+  //     width: dimensions?.width ? dimensions?.width : 0,
+  //     height: dimensions?.height ? dimensions?.height : 0,
+  //   };
+  // } else {
+  //   throw new AuthenticationError(`You don't have permission!`);
+  // }
 };
