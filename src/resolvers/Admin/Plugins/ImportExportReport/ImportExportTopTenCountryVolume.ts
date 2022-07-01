335ec287d705985de9826c8e616d3a63d30c3c8d
Reachea <reacheasambath@gmail.com>
2022-07-01 15:27:50 +0700

    added country name to top 10 import export

--- src/resolvers/Admin/Plugins/ImportExportReport/ImportExportTopTenCountryVolume.ts
+++ src/resolvers/Admin/Plugins/ImportExportReport/ImportExportTopTenCountryVolume.ts
@@ -5,8 +5,9 @@ export const ImportExportTopTenCountryVolume = async (_, {}, ctx: ContextType) =
 
   const imports_detail = await knex.raw(
     `
-    SELECT ed.origin_country as country, sum(ed.custom_value_usd) as volume
+    SELECT ed.origin_country as country, sum(ed.custom_value_usd) as volume, sc.country_name as country_name
     FROM imports_detail AS ed
+    INNER JOIN stat_countries as sc ON sc.code = ed.origin_country
     GROUP BY ed.origin_country
     ORDER BY Sum(ed.custom_value_usd) DESC LIMIT 10
     `,
@@ -14,8 +15,9 @@ export const ImportExportTopTenCountryVolume = async (_, {}, ctx: ContextType) =
 
   const exports_detail = await knex.raw(
     `
-    SELECT ed.destination_country as country, sum(ed.custom_value_usd) as volume
+    SELECT ed.destination_country as country, sum(ed.custom_value_usd) as volume, sc.country_name as country_name
     FROM exports_detail AS ed
+    INNER JOIN stat_countries as sc ON sc.code = ed.destination_country
     GROUP BY ed.destination_country
     ORDER BY Sum(ed.custom_value_usd) DESC LIMIT 10
     `,
@@ -23,7 +25,7 @@ export const ImportExportTopTenCountryVolume = async (_, {}, ctx: ContextType) =
 
   const volumes_detail = await knex.raw(
     `
-    select country,sum(custom_value_usd) AS volume
+    select country,sum(custom_value_usd) AS volume, sc.country_name as country_name
     from
     (
         select imports_detail.origin_country AS country ,imports_detail.custom_value_usd
@@ -32,6 +34,7 @@ export const ImportExportTopTenCountryVolume = async (_, {}, ctx: ContextType) =
         select exports_detail.destination_country AS country, exports_detail.custom_value_usd
         from exports_detail
     ) t
+    INNER JOIN stat_countries as sc ON sc.code = country
     group by country ORDER BY volume desc
     LIMIT 10
     `,
