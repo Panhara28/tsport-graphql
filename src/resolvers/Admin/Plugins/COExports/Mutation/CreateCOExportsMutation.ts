48f45d3cdfb71b71eef51134da541cd19958a60d
Reachea <reacheasambath@gmail.com>
2022-07-13 14:08:27 +0700

    change normal insert to transaction insert in gql

--- src/resolvers/Admin/Plugins/COExports/Mutation/CreateCOExportsMutation.ts
+++ src/resolvers/Admin/Plugins/COExports/Mutation/CreateCOExportsMutation.ts
@@ -11,26 +11,25 @@ export const CreateCOExportsMutation = async (_, { input }: { input: Graph.COExp
 
   if (createCOExports[0] > 0) {
     try {
-      for (const x of input?.data) {
-        await knex.table('co_exports_detail').insert({
-          year: x?.year,
-          month: x?.month,
-          destination_country: x?.destination_country,
-          hs_code: x?.hs_code,
-          unit: x?.unit,
-          quantity: x?.quantity,
-          fob_value_usd: x?.fob_value_usd,
-          form_type: x?.form_type,
-          co_exports_id: createCOExports[0],
+      await knex.transaction(async (trx: any) => {
+        let inputData = input?.data?.map(x => {
+          return {
+            year: x?.year,
+            month: x?.month,
+            destination_country: x?.destination_country,
+            hs_code: x?.hs_code,
+            unit: x?.unit,
+            quantity: x?.quantity,
+            fob_value_usd: x?.fob_value_usd,
+            form_type: x?.form_type,
+            co_exports_id: createCOExports[0],
+          };
         });
-      }
-    } catch (err) {
-      const message = {
-        message: 'Upload Failed!',
-        coExportId: createCOExports[0],
-      };
 
-      throw new Error(JSON.stringify(message));
+        await trx('co_exports_detail').insert(inputData);
+      });
+    } catch (err) {
+      console.error(err);
     }
 
     return createCOExports[0];
