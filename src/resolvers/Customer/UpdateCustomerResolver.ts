486f0bb728cab18760f0e9145407ff45ed471951
Sokphal Adam <sokphaladam@gmail.com>
2022-10-13 23:25:35 +0700

    fix

--- src/resolvers/Customer/UpdateCustomerResolver.ts
+++ src/resolvers/Customer/UpdateCustomerResolver.ts
@@ -1,11 +1,12 @@
 import ContextType from 'src/graphql/ContextType';
 import md5 from 'md5';
+import { table_customers } from 'src/generated/tables';
 
 export async function UpdateCustomerResolver(_: any, { id, data }: any, ctx: ContextType): Promise<boolean> {
   const knex = ctx.knex.default;
   const user = ctx.authCustomer;
 
-  const customer = await knex
+  const customer: table_customers = await knex
     .table('customers')
     .where({ id })
     .first();
@@ -21,12 +22,12 @@ export async function UpdateCustomerResolver(_: any, { id, data }: any, ctx: Con
   }
 
   const customers = await query.clone().update({
-    display: data.fullname,
-    phone: data.phone,
-    type: data.type,
-    address: data.address,
-    discount: data.discount,
-    profile: data.profile,
+    display: data.fullname ? data.fullname : customer.display,
+    phone: data.phone ? data.phone : customer.phone,
+    type: data.type ? data.type : customer.type,
+    address: data.address ? data.address : customer.address,
+    discount: data.discount ? data.discount : customer.discount,
+    profile: data.profile ? data.profile : customer.profile,
     username: data.username ? data.username : customer.username,
     password: data.password
       ? data.password === customer.password
