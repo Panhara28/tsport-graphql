e6edc22f312e03b04e88cbb63c944dbd76f31fff
Sokphal Adam <sokphaladam@gmail.com>
2023-03-04 12:37:31 +0700

    fix

--- src/resolvers/Upload/Mutation/UploadResolver.ts
+++ src/resolvers/Upload/Mutation/UploadResolver.ts
@@ -1,6 +1,19 @@
 import ContextType from '../../../graphql/ContextType';
 import FormData from 'form-data';
 import fetch from 'node-fetch';
+import fs from 'fs';
+
+async function RestUpload(form: FormData) {
+  const res = await fetch(process.env.S1 ? process.env.S1 + '/upload' : 'https://s1.tsportcambodia.com' + '/upload', {
+    method: 'POST',
+    body: form,
+    headers: {
+      Authorization: 'Bearer ' + process.env.AUTHORIZATION,
+    },
+  });
+
+  return await res.json();
+}
 
 export const UploadResolver = async (_, { file }, ctx: ContextType) => {
   // await ctx.authUser.requireLogin('USER');
@@ -18,7 +31,11 @@ export const UploadResolver = async (_, { file }, ctx: ContextType) => {
 
   const form = new FormData();
 
-  form.append('mocspace', stream, filename);
+  form.append('mocspace', 'hello');
+
+  const x = await RestUpload(form);
+  console.log(x);
+  // form.append('mocspace', stream, filename);
 
   const res = await fetch(process.env.S1 ? process.env.S1 + '/upload' : 'https://s1.tsportcambodia.com' + '/upload', {
     method: 'POST',
@@ -27,7 +44,6 @@ export const UploadResolver = async (_, { file }, ctx: ContextType) => {
       Authorization: 'Bearer ' + process.env.AUTHORIZATION,
     },
   });
-  console.log('res', res);
 
   const json: any = await res.json();
 
